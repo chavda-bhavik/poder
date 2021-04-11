f6a2726664e84df665ed23fa8d81df543b97a63b
chavda-bhavik <bhavik.chvda@gmail.com>
2021-04-11 10:54:55 +0530

    added audio player

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Episode from './components/Episode';
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
+import './style.css';
 
 function App() {
   return (
     <div className="h-full bg-gradient-to-br from-gray-600 via-teal-7 to-gray-800 p-3">
-      <div className="w-full h-full bg-white rounded shadow-md">
-        
+      
+      <div className="w-full h-full bg-white rounded-md">  
         <form method="GET">
-          <div className="relative bg-green-200 rounded-t-sm p-1 border-b-2 border-gray-800">
+          <div className="relative bg-green-200 rounded-t-md p-1 border-b-2 border-gray-800">
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
               <button type="submit" className="p-1 focus:outline-none focus:shadow-outline text-gray-800">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
@@ -24,7 +27,7 @@ function App() {
           </div>
         </form>
 
-        <div className="overflow-y-auto" style={{ height: 'calc(100% - 60px)' }}>
+        <div className="overflow-y-auto" style={{ height: 'calc(100% - 100px)' }}>
           <Episode
             imageAlt="image of Star Wars - The Force Awakens"
             imageSrc="https://cdn-images-1.listennotes.com/podcasts/the-rough-cut-AzKVtPeMOL4-53MLh7NpAwm.300x300.jpg"
@@ -98,6 +101,14 @@ function App() {
             duration={1694}
             title="Star Wars - The Force Awakens The Force Awakens The Force Awakens The Force Awakens"
             key={9}
+          />
+        </div>
+        
+        <div className="bottom-0">
+          <AudioPlayer
+            className="rounded-md"
+            layout="horizontal-reverse"
+            src="https://www.listennotes.com/e/p/6280a11466dd407e99c66130f203167a/"
           />
         </div>
       </div>
